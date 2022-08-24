c45ff9989678a52850d1f8029fe11e3734b943e6
Natalia Pinzon <nanuta522@gmail.com>
2022-08-24 12:43:01 +0200

    first commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,10 @@
+import 'bootstrap'
+import 'css/styles'
+
+/* import "@hotwired/turbo-rails";
+import "controllers";
+import "trix";
+import "@rails/actiontext";
+import "chartkick";
+import "Chart.bundle";
+ */
